0919045f58e81c19475f4affa92e99e99b2d5bd5
15521648013a <528078704@qq.com>
2020-06-12 08:58:01 +0800

    update

--- resources/views/public/head.blade.php
+++ resources/views/public/head.blade.php
@@ -7,12 +7,12 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Sj</title>
-		<link rel="stylesheet" type="text/css" href="/MyBlog/public/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css')}}">
 		<!-- <link rel="stylesheet" type="text/css" href="/MyBlog/public/css/nprogress.css"> -->
-		<link rel="stylesheet" type="text/css" href="/MyBlog/public/css/style.css">
-		<link rel="stylesheet" type="text/css" href="/MyBlog/public/css/font-awesome.min.css">
-		<link rel="apple-touch-icon-precomposed" href="/MyBlog/public/img/icon/icon.png">
-		<link rel="shortcut icon" href="/MyBlog/public/img/icon/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css')}}">
+		<link rel="apple-touch-icon-precomposed" href="{{ asset('img/icon/icon.png')}}">
+		<link rel="shortcut icon" href="{{ asset('img/icon/favicon.ico')}}">
 		<script src="/MyBlog/public/js/jquery-2.1.4.min.js"></script>
 		<!-- <script src="js/nprogress.js"></script> -->
 		<!-- <script src="js/jquery.lazyload.min.js"></script> -->
@@ -25,8 +25,8 @@
 		<!--[if lt IE 9]>
   <script>window.location.href='upgrade-browser.html';</script>
 <![endif]-->
-        <script src="/MyBlog/public/js/bootstrap.min.js"></script>
-		<script src="/MyBlog/public/js/jquery.ias.js"></script>
+        <script src="{{ asset('js/bootstrap.min.js')}}"></script>
+		<script src="{{ asset('js/jquery.ias.js')}}"></script>
 	</head>
 	<body class="user-select">
 		<header class="header">
@@ -34,17 +34,17 @@
 				<div class="container">
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#header-navbar" aria-expanded="false"> <span class="sr-only"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-						<h1 class="logo hvr-bounce-in"><a href="/MyBlog/public/admin/article" title=""><img src="/MyBlog/public/img/timg.jpg" alt="" style="height: 60px;"></a></h1>						
+						<h1 class="logo hvr-bounce-in"><a href="{{ asset('admin/article')}}" title=""><img src="{{asset('img/timg.jpg')}}" alt="" style="height: 60px;"></a></h1>						
 					</div>
 					<div class="collapse navbar-collapse" id="header-navbar">
 						<ul class="nav navbar-nav navbar-right">
 							<li class="hidden-index {{isset($category)?'':'active'}} ">
-								<a data-cont="Sj" href="/MyBlog/public/">首页</a>
+								<a data-cont="Sj" href="{{ asset('')}}">首页</a>
 							</li>
 							<?php $cat=isset($category)?$category:'';?>
                             @foreach ($categories as $category)
 		 					<li class="{{$cat==$category->category_id ?'active':''}}">
-		                        <a href="/MyBlog/public/articleCategory/{{$category->category_id}}">{{$category->category_name}}</a>
+		                        <a href="{{ asset('articleCategory')}}/{{$category->category_id}}">{{$category->category_name}}</a>
 							</li>
 							@endforeach
 							<li class="dropdown">
@@ -52,9 +52,9 @@
 							    	{{session('username')}}
 							    	<span class="caret"></span></a>
 							    <ul class="dropdown-menu">
-								    <li><a href="/MyBlog/public/login">登录</li>
+								    <li><a href="{{ asset('login')}}">登录</li>
 							    	<li><a href="register.php">注册</a></li>
-							    	<li><a href="/MyBlog/public/loginOut">退出</a></li>
+							    	<li><a href="{{ asset('loginOut')}}">退出</a></li>
 							    </ul>
 						    </li>
 						</ul>
